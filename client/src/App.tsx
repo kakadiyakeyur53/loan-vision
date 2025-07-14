d1992a0286f10b0956fa37213bd938bb1da1af2f
keyurkakadiya53 <67643835+kakadiyakeyur53@users.noreply.github.com>
2025-07-14 23:30:22 +1000

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={LoanCalculator} />
-      <Route component={NotFound} />
+      <Route component={LoanCalculator} />
     </Switch>
   );
 }
